8bda651d8ce804881a171c020c4a8d6d83bce0df
KarlaJMT <2302091@utrivieramaya.edu.mx>
2025-07-18 07:58:18 -0500

    actualizacion de controllers

--- src/controllers/career.controller.js
+++ src/controllers/career.controller.js
@@ -5,15 +5,24 @@ export const getAllCareers = async (req, res) => {
         const careers = await Career.find();
         res.json(careers);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        res.status(400).json({ message: error.message });
     }
 };
 
 export const createCareer = async (req, res) => {
     try {
-        const newCareer = new Career(req.body);
+
+        const newCareer = new Career({
+            career_code,
+            career_name,
+            description,
+            four_quarter_duration,
+            modality,
+            creation_date,
+            active
+        })
         const savedCareer = await newCareer.save();
-        res.status(201).json(savedCareer);
+        res.status(200).json(savedCareer);
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
@@ -22,10 +31,10 @@ export const createCareer = async (req, res) => {
 export const getCareerById = async (req, res) => {
     try {
         const career = await Career.findById(req.params.id);
-        if (!career) return res.status(404).json({ message: 'Career not found' });
+        if (!career) return res.status(404).json({ message: 'Carrera no encontrada.' });
         res.json(career);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        res.status(400).json({ message: error.message });
     }
 };
 
@@ -34,19 +43,20 @@ export const updatedCareer = async (req, res) => {
         const career = await Career.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
         });
-        if (!career) return res.status(404).json({ message: 'Career not found' });
+        if (!career) return res.status(404).json({ message: 'Carrera no encontrada.' });
         res.json(career);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        res.status(400).json({ message: error.message });
     }
 };
 
 export const deleteCareer = async (req, res) => {
     try {
         const career = await Career.findByIdAndDelete(req.params.id);
-        if (!career) return res.status(404).json({ message: 'Career not found' });
-        res.sendStatus(204);
+        if (!career) return res.status(404).json({ message: 'Carrera no encontrada.' });
+        res.json(career);
+        // res.sendStatus(200);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        res.status(400).json({ message: error.message });
     }
 };
